0fcab9f2828a465fa0d3cf52c05384c359532986
sa-ku <30826464+sa-ku@users.noreply.github.com>
2017-08-16 11:31:49 +0530

    Update ssh_router.py

--- TESTCASES/ssh_router.py
+++ TESTCASES/ssh_router.py
@@ -10,5 +10,6 @@ def TEST_CASE_001():
     log.rite("Starting Testcase 001\n")
     connect_to_cisco_device = ssh_connection(devices['DEVICE_1'])
     print connect_to_cisco_device.show_command('show version')
+    log.rite(str(connect_to_cisco_device.show_command('show version')))
     
     
