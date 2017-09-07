a017ac394896ca4b7a39c4e4f3553ff360d7b1ea
sa-ku <30826464+sa-ku@users.noreply.github.com>
2017-09-07 11:50:42 +0530

    Update package_list.sh
    issue #14 fixed

--- src/libs/Traffic/package_list.sh
+++ src/libs/Traffic/package_list.sh
@@ -15,7 +15,9 @@ cd ..
 wget http://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9
 tar xvzf pip-9.0.1.tar.gz
 cd pip-9.0.1
-python setup.py install
+version=$(which python 2>&1)
+$version setup.py install
+#python setup.py install
 
 #Install scapy
 pip install scapy
