7c23ec7f455126fec658d8e5300eef19743b102e
root <kavip@aricent.com>
2017-11-05 11:33:08 -0600

    code added

--- TESTCASES/snmp_example_walk.py
+++ TESTCASES/snmp_example_walk.py
@@ -9,12 +9,25 @@ from src.libs.snmp.SNMP import *
 from src.libs.logging.print_log import *
 
 def TEST_CASE_001():
-    obj = snmpwalker(devices['DEVICE_1'])
-    op = obj.snmpwalk(['.1.3.6.1.2.1.1'])
-    for i in op:
-         Print(i,log)
+    try:
+        obj = snmpwalker(devices['DEVICE_1'])
+        op = obj.snmpwalk(['.1.3.6.1.2.1.1'])
+        for i in op:
+             Print(i,log)
+    except Exception as err:
+	Print(err,log)
     return 1
 
+
+def TEST_CASE_002():
+    try:
+        obj = snmpwalker(devices['DEVICE_1'])
+        op = obj.snmpwalk(['.1.3.6.1.2.1.1'])
+        for i in op:
+             Print(i,log)
+    except Exception as err:
+	Print(err,log)
+    return 1
 
 '''
 NOTE:-
