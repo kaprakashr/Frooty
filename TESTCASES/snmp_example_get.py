9e979ed33ec26a092e65d9b42e8c5fe64f2a321b
root <kavip@aricent.com>
2017-11-05 11:34:02 -0600

    code added

--- TESTCASES/snmp_example_get.py
+++ TESTCASES/snmp_example_get.py
@@ -8,6 +8,13 @@ from src.libs.string.verify import *
 from src.libs.snmp.SNMP import *
 from src.libs.logging.print_log import *
 
+def TEST_CASE_001():
+    obj = snmpwalker(devices['DEVICE_1'])
+    op = obj.snmpget(['1.3.6.1.2.1.1.1.0', '1.3.6.1.2.1.1.6.0'])
+    for i in op:
+        Print(i,log)
+    return 1
+
 def TEST_CASE_002():
     obj = snmpwalker(devices['DEVICE_1'])
     op = obj.snmpget(['1.3.6.1.2.1.1.1.0', '1.3.6.1.2.1.1.6.0'])
@@ -15,9 +22,21 @@ def TEST_CASE_002():
         Print(i,log)
     return 1
 
+def TEST_CASE_003():
+    obj = snmpwalker(devices['DEVICE_1'])
+    op = obj.snmpget(['1.3.6.1.2.1.1.1.0', '1.3.6.1.2.1.1.6.0'])
+    for i in op:
+        Print(i,log)
+    return 1
+
+def TEST_CASE_004():
+    obj = snmpwalker(devices['DEVICE_1'])
+    op = obj.snmpget(['1.3.6.1.2.1.1.1.0', '1.3.6.1.2.1.1.6.0'])
+    for i in op:
+        Print(i,log)
+    return 1
 
 '''
-NOTE:-
 SNMP MIB OID name not supporting, So Use MIB OID number for snmpwalk/snmpget/snmpset operation
 and also SNMPv1 and SNMPv2c currently supported
 '''
